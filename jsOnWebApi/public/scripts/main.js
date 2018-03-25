15165d651e47bf6f77b9714cccddf2d6e892a407
yongqin wang <jcyongqin@outlook.com>
2018-03-25 18:55:36 +0800

    html with css; js startup

---
+++ jsOnWebApi/public/scripts/main.js
@@ -0,0 +1,8 @@
+function multiply(num1,num2) {
+    var result = num1 * num2;
+    return result;
+  }
+
+  document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+} 
